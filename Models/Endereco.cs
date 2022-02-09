5b16dcb42bc34fe5fa5b2a7a26cb85ab96073f84
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-02-09 11:38:42 -0300

    Initial commit

---
+++ Models/Endereco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CepApp.Models
+{
+    public class Endereco
+    {
+        public string Logradouro { get; set; }
+        public string Cep { get; set; }
+        public string Bairro { get; set; }
+        public string Localidade { get; set; }
+        public string Uf { get; set; }
+
+    }
+}
